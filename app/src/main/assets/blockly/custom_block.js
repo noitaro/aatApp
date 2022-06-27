d4e4eb29e3cfee1bd54866aea73380f7a43e85f4
茄子紺 <52857466+noitaro@users.noreply.github.com>
2022-06-27 22:18:25 +0900

    ブロックを統一

--- app/src/main/assets/blockly/custom_block.js
+++ app/src/main/assets/blockly/custom_block.js
@@ -1,73 +1,119 @@
-Blockly.Blocks['text_log'] = {
-  init: function() {
-    this.appendValueInput("TEXT")
-        .setCheck("String")
-        .appendField("log");
+Blockly.Blocks['screen_capture'] = {
+  init: function () {
+    this.appendDummyInput()
+      .appendField("アンドロイドのスクリーンショットを撮影");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
-    this.setColour(160);
- this.setTooltip("");
- this.setHelpUrl("");
+    this.setColour(65);
+  }
+};
+
+Blockly.Blocks['tap'] = {
+  init: function () {
+    this.appendValueInput("NAME")
+      .setCheck(null)
+      .appendField("スクリーンショット内をタップ");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(65);
+  }
+};
+
+Blockly.Blocks['field_image_serializable'] = {
+  init: function () {
+    this.appendDummyInput()
+      .appendField(new Blockly.FieldImageSerializable('data1'), 'FIELDNAME')
+      .appendField(new Blockly.FieldTextInput("default"), "NAME");
+    this.setOutput(true, null);
+    this.setColour(65);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['sleep'] = {
+  init: function () {
+    this.appendDummyInput()
+      .appendField(new Blockly.FieldNumber(3, 0), "NAME")
+      .appendField("秒間停止します");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(65);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['text_log'] = {
+  init: function () {
+    this.appendValueInput("TEXT")
+      .setCheck("String")
+      .appendField("log");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(65);
+    this.setTooltip("");
+    this.setHelpUrl("");
   }
 };
 
 Blockly.Blocks['controls_sleep'] = {
-  init: function() {
+  init: function () {
     this.appendValueInput("NUM")
-        .setCheck("Number")
-        .appendField("sleep");
+      .setCheck("Number")
+      .appendField("sleep");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
-    this.setColour(120);
- this.setTooltip("");
- this.setHelpUrl("");
+    this.setColour(65);
+    this.setTooltip("");
+    this.setHelpUrl("");
   }
 };
 
 Blockly.Blocks['device_tap'] = {
-  init: function() {
+  init: function () {
     this.appendDummyInput()
-        .appendField("tap");
+      .appendField("tap");
     this.appendValueInput("X")
-        .setCheck("Number")
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField("x");
+      .setCheck("Number")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField("x");
     this.appendValueInput("Y")
-        .setCheck("Number")
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField("y");
+      .setCheck("Number")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField("y");
     this.appendDummyInput();
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
+    this.setColour(65);
+    this.setTooltip("");
+    this.setHelpUrl("");
   }
 };
 
 Blockly.Blocks['text_toast'] = {
-  init: function() {
+  init: function () {
     this.appendValueInput("TEXT")
-        .setCheck("String")
-        .appendField("toast");
+      .setCheck("String")
+      .appendField("toast");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
-    this.setColour(160);
- this.setTooltip("");
- this.setHelpUrl("");
+    this.setColour(65);
+    this.setTooltip("");
+    this.setHelpUrl("");
   }
 };
 
 Blockly.Blocks['device_key'] = {
-  init: function() {
+  init: function () {
     this.appendDummyInput()
-        .appendField("Press the ")
-        .appendField(new Blockly.FieldDropdown([["home","3"], ["back","4"], ["camera","27"], ["app switch","187"], ["menu","82"], ["power","26"], ["settings","176"], ["volume down","25"], ["volume mute","164"], ["volume up","24"], ["screenshot","120"]]), "NAME")
-        .appendField("button");
+      .appendField("Press the ")
+      .appendField(new Blockly.FieldDropdown([["home", "3"], ["back", "4"], ["camera", "27"], ["app switch", "187"], ["menu", "82"], ["power", "26"], ["settings", "176"], ["volume down", "25"], ["volume mute", "164"], ["volume up", "24"], ["screenshot", "120"]]), "NAME")
+      .appendField("button");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
+    this.setColour(65);
+    this.setTooltip("");
+    this.setHelpUrl("");
   }
 };
--- app/src/main/assets/blockly/custom_generator.js
+++ app/src/main/assets/blockly/custom_generator.js
@@ -1,35 +1,53 @@
-Blockly.Lua['text_log'] = function(block) {
+Blockly.Lua['screen_capture'] = function (block) {
+  var code = 'AdbScreenCapture();\n';
+  return code;
+};
+
+Blockly.Lua['tap'] = function (block) {
+  var value_name = Blockly.Lua.valueToCode(block, 'NAME', Blockly.Lua.ORDER_NONE);
+  var code = 'AdbTap(' + value_name + ');\n';
+  return code;
+};
+
+Blockly.Lua['field_image_serializable'] = function (block) {
+  var name = block.getFieldValue('FIELDNAME');
+  var code = '"' + name + '"';
+  return [code, Blockly.Lua.ORDER_ATOMIC];
+};
+
+Blockly.Lua['sleep'] = function (block) {
+  var number_name = block.getFieldValue('NAME');
+  var code = 'AdbSleep(' + number_name + ');\n';
+  return code;
+};
+
+Blockly.Lua['text_log'] = function (block) {
   var value_text = Blockly.Lua.valueToCode(block, 'TEXT', Blockly.Lua.ORDER_ATOMIC);
-  // TODO: Assemble Lua into code variable.
   var code = 'MyLua2Java.log(' + value_text + ');\n';
   return code;
 };
 
-Blockly.Lua['controls_sleep'] = function(block) {
+Blockly.Lua['controls_sleep'] = function (block) {
   var value_num = Blockly.Lua.valueToCode(block, 'NUM', Blockly.Lua.ORDER_ATOMIC);
-  // TODO: Assemble Lua into code variable.
   var code = 'MyLua2Java.sleep(' + value_num + ');\n';
   return code;
 };
 
-Blockly.Lua['device_tap'] = function(block) {
+Blockly.Lua['device_tap'] = function (block) {
   var value_x = Blockly.Lua.valueToCode(block, 'X', Blockly.Lua.ORDER_ATOMIC);
   var value_y = Blockly.Lua.valueToCode(block, 'Y', Blockly.Lua.ORDER_ATOMIC);
-  // TODO: Assemble Lua into code variable.
   var code = 'MyLua2Java.deviceTap(' + value_x + ', ' + value_y + ');\n';
   return code;
 };
 
-Blockly.Lua['text_toast'] = function(block) {
+Blockly.Lua['text_toast'] = function (block) {
   var value_text = Blockly.Lua.valueToCode(block, 'TEXT', Blockly.Lua.ORDER_ATOMIC);
-  // TODO: Assemble Lua into code variable.
   var code = 'MyLua2Java.toast(' + value_text + ');\n';
   return code;
 };
 
-Blockly.Lua['device_key'] = function(block) {
+Blockly.Lua['device_key'] = function (block) {
   var dropdown_name = block.getFieldValue('NAME');
-  // TODO: Assemble Lua into code variable.
   var code = 'MyLua2Java.deviceKey(' + dropdown_name + ');\n';
   return code;
 };
